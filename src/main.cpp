7d72c9e976b801ff23f48fea76a5b2cdebb4ba61
Digitql0 <Digitql0@users.noreply.github.com>
2025-04-28 16:20:26 +0200

    Basis commands implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include "Terminal.h"
-
-using namespace std;
+#include <string>
 
 int main()
 {
-    cout << "Hello World" << endl;
+    std::cout << "Program started." << std::endl;
     Terminal terminal;
+
+    while (true)
+    {
+        std::string command;
+        std::cout << "$ ";
+        std::getline(std::cin, command);
+        // std::cout << std::endl;
+        int syscall = terminal.handleCommand(command);
+        std::cout << "-//  " << syscall << std::endl;
+
+        if (syscall == 961)
+        {
+            std::cout << "Too many Arguments were given." << std::endl;
+        }
+
+        if (syscall == 962)
+        {
+            std::cout << "Not enough Arguments were given." << std::endl;
+        }
+
+        if (syscall == 1)
+        {
+            std::cout << "Ending Terminal..." << std::endl;
+            break;
+        }
+    }
+
     return 0;
 }
